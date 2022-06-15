f7ad30552e6535021076119a3ff95943213c4d9d
ChineduG <investorgbriel@gmail.com>
2022-06-15 11:03:06 +0100

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const PORT = process.env.PORT || 3000
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on localhost:${PORT}`);
+})
