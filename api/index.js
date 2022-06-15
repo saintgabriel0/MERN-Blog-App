77b45110f6e75e0a65b1e848a36e5159b8b6c6da
ChineduG <investorgbriel@gmail.com>
2022-06-15 12:00:06 +0100

    changes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000
 const app = express();
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv')
+app.use('/ Money', (req, res)=>{})
+
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL, {
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 app.listen(PORT, () => {
     console.log(`Server is running on localhost:${PORT}`);
