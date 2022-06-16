0ef96ac36b05eee1d03ce363763ea3c3529e4154
ChineduG <investorgbriel@gmail.com>
2022-06-16 08:20:59 +0100

    auth

--- api/index.js
+++ api/index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000
 const app = express();
-const mongoose = require('mongoose');
-
 const dotenv = require('dotenv')
-app.use('/ Money', (req, res)=>{})
+const mongoose = require('mongoose');
+const authRoute = require('./routes/auth')
 
 
 dotenv.config();
+app.use(express.json());
+
 mongoose
   .connect(process.env.MONGO_URL, {
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
+
+app.use('/api/auth', authRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is running on localhost:${PORT}`);
